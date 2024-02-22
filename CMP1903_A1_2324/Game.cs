aed1e298e134fc9e58808d67a5d5e6889732c4f0
Jake Mitchell <j.mitchell7574@gmail.com>
2024-02-22 15:28:57 +0000

    Updated some comments

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -8,13 +8,6 @@ namespace CMP1903_A1_2324
 {
     internal class Game
     {
-        /*
-         * The Game class should create three die objects, roll them, sum and report the total of the three dice rolls.
-         *
-         * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
-         * rolls could be continous, and the totals and other statistics could be summarised for example.
-         */
-
         // Dice Objects
         private readonly Die _die1 = new Die();
         private readonly Die _die2 = new Die();
